91c7d598658226cfcaf0d2b1c0f3d8c9cd5b9c00
ScarletteBel <83143055+ScarletteBel@users.noreply.github.com>
2023-02-11 15:15:47 -0500

    linear scaling compared

---
+++ DiabetesNumericalDataScaling/DiabetesDataScaling.py
@@ -0,0 +1,41 @@
+### Scaling data
+# This excersice is to compare and validate the effect of transforming data before apllying a model 
+
+
+import timeit # measuring model's performance (execution time with transformed data or original data)
+import numpy as no 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from sklearn import datasets, linear_model
+
+
+X, y = datasets.load_diabetes(return_X_y=True)
+raw = X[:, None, 2]  #Adjusting dimentions for preprocessing 
+
+
+## Data Scaling Rules 
+max_raw = max(raw)
+min_raw = min(raw)
+scaled = (2*raw - max_raw - min_raw)/(max_raw - min_raw)
+
+fig, axs = plt.subplots(2, 1, sharex= True)
+
+axs[0].hist(raw)
+axs[1].hist(scaled)
+plt.show()
+
+
+## Training models 
+def train_raw():
+    linear_model.LinearRegression().fit(raw, y)
+
+def trained_scaled():
+    linear_model.LinearRegression().fit(scaled, y)
+
+
+##Comparing processing times 
+raw_time = timeit.timeit(train_raw, number= 100)
+scaled_time = timeit.timeit(trained_scaled, number= 100)
+
+print('train raw: {}'.format(raw_time))
+print('train scaled: {}'.format(scaled_time))
